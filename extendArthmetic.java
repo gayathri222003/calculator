91d894c7ebbdc9879977013064f257908255ffbe
gayathri222003 <01fe22bca039@kletech.ac.in>
2024-02-19 16:02:31 +0530

    code for Extended arhmetics

---
+++ extendArthmetic.java
@@ -0,0 +1,9 @@
+public class extendArthmetic extends cal {
+    void squre(int b){
+        System.out.println("Squre of a number is =" +b*b);
+    }
+    void cube(int b){
+        System.out.println("Squre of a number is =" +b*b*b);
+    }
+}
+
